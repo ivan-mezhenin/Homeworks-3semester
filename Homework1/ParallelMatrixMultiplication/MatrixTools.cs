57aa43b4e0419eb26e26a4697a138de9d81234cf
ivan-mezhenin <mezhenin_ivan_06@mail.ru>
2025-09-15 15:09:53 +0300

    feat: add ParallelMatrixMultiplication and MatrixMultiplication

--- Homework1/ParallelMatrixMultiplication/MatrixTools.cs
+++ Homework1/ParallelMatrixMultiplication/MatrixTools.cs
@@ -60,4 +60,96 @@ public static class MatrixTools
 
         return matrix;
     }
+
+    /// <summary>
+    /// to multiply matrix.
+    /// </summary>
+    /// <param name="matrix1">first matrix.</param>
+    /// <param name="matrix2">second matrix.</param>
+    /// <returns>result matrix of multiplication.</returns>
+    public static int[,] MultiplyMatrix(int[,] matrix1, int[,] matrix2)
+    {
+        var rows1 = matrix1.GetLength(0);
+        var cols1 = matrix1.GetLength(1);
+        var rows2 = matrix2.GetLength(0);
+        var cols2 = matrix2.GetLength(1);
+
+        if (cols1 != rows2 || rows1 != cols2)
+        {
+            throw new FormatException("It is impossible to multiply matrices of such sizes");
+        }
+
+        var resultMatrix = new int[rows1, cols2];
+
+        for (var row = 0; row < rows1; row++)
+        {
+            for (var col = 0; col < cols2; col++)
+            {
+                var sum = 0;
+
+                for (var k = 0; k < cols1; k++)
+                {
+                    sum += matrix1[row, k] * matrix2[k, col];
+                }
+
+                resultMatrix[row, col] = sum;
+            }
+        }
+
+        return resultMatrix;
+    }
+
+    /// <summary>
+    /// to parallel multiply matrix.
+    /// </summary>
+    /// <param name="matrix1">first matrix.</param>
+    /// <param name="matrix2">second matrix.</param>
+    /// <returns>result matrix of multiplication.</returns>
+    public static int[,] ParallelMultiplyMatrix(int[,] matrix1, int[,] matrix2)
+    {
+        var rows1 = matrix1.GetLength(0);
+        var cols1 = matrix1.GetLength(1);
+        var rows2 = matrix2.GetLength(0);
+        var cols2 = matrix2.GetLength(1);
+
+        if (cols1 != rows2 || rows1 != cols2)
+        {
+            throw new FormatException("It is impossible to multiply matrices of such sizes");
+        }
+
+        var resultMatrix = new int[rows1, cols2];
+        var threads = new Thread[rows1];
+
+        for (var row = 0; row < rows1; row++)
+        {
+            var localRow = row;
+
+            threads[row] = new Thread(() =>
+            {
+                for (var col = 0; col < cols2; col++)
+                {
+                    var sum = 0;
+
+                    for (var k = 0; k < cols1; k++)
+                    {
+                        sum += matrix1[localRow, k] * matrix2[k, col];
+                    }
+
+                    resultMatrix[localRow, col] = sum;
+                }
+            });
+        }
+
+        foreach (var thread in threads)
+        {
+            thread.Start();
+        }
+
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+
+        return resultMatrix;
+    }
 }
--- Homework1/ParallelMatrixMultiplication/Program.cs
+++ Homework1/ParallelMatrixMultiplication/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// <copyright file="Program.cs" company="ivan-mezhenin">
+// Copyright (c) ivan-mezhenin. All rights reserved.
+// </copyright>
+
+using ParallelMatrixMultiplication;
+
+var matrix1Path = args[0];
+var matrix2Path = args[1];
+
+var matrix1 = MatrixTools.ReadMatrixFromFile(matrix1Path);
+var matrix2 = MatrixTools.ReadMatrixFromFile(matrix2Path);
+
+var matrix = MatrixTools.MultiplyMatrix(matrix1, matrix2);
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j],4}");
+    }
+
+    Console.WriteLine();
+}
+

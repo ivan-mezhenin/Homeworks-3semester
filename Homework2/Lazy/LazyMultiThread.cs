95abbfd9b52829f5776ef2153ad2f0a984399255
ivan-mezhenin <mezhenin_ivan_06@mail.ru>
2025-09-17 16:15:37 +0300

    feat: LazyMultiThread

--- Homework2/Lazy/LazyMultiThread.cs
+++ Homework2/Lazy/LazyMultiThread.cs
@@ -1,24 +1,26 @@
-// <copyright file="LazySingleThread.cs" company="ivan-mezhenin">
+// <copyright file="LazyMultiThread.cs" company="ivan-mezhenin">
 // Copyright (c) ivan-mezhenin. All rights reserved.
 // </copyright>
 
 namespace Lazy;
 
 /// <summary>
-/// lazy calculation of type T for single thread.
+/// lazy calculation of type T for multi thread.
 /// </summary>
 /// <typeparam name="T">The type of value to be calculated.</typeparam>
-public class LazySingleThread<T> : ILazy<T>
+public class LazyMultiThread<T> : ILazy<T>
 {
+    private readonly object lockObject = new();
+
     private Func<T>? supplier;
     private T? value;
     private bool isValueCalculated;
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="LazySingleThread{T}"/> class.
+    /// Initializes a new instance of the <see cref="LazyMultiThread{T}"/> class.
     /// </summary>
     /// <param name="supplier">the function that calculates the value.</param>
-    public LazySingleThread(Func<T> supplier)
+    public LazyMultiThread(Func<T> supplier)
     {
         this.supplier = supplier ?? throw new ArgumentNullException(nameof(supplier));
         this.isValueCalculated = false;
@@ -29,9 +31,15 @@ public class LazySingleThread<T> : ILazy<T>
     {
         if (!this.isValueCalculated)
         {
-            this.value = this.supplier!();
-            this.supplier = null;
-            this.isValueCalculated = true;
+            lock (this.lockObject)
+            {
+                if (!this.isValueCalculated)
+                {
+                    this.value = this.supplier!();
+                    this.supplier = null;
+                    this.isValueCalculated = true;
+                }
+            }
         }
 
         return this.value!;

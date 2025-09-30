07e7f9fdc41bfc3b6fb89d228dad5369566b5f05
ivan-mezhenin <mezhenin_ivan_06@mail.ru>
2025-09-30 18:18:59 +0300

    feat: IMyTask.cs

---
+++ Homework3/ThreadPool/MyThreadPool.cs
@@ -0,0 +1,9 @@
+// <copyright file="MyThreadPool.cs" company="ivan-mezhenin">
+// Copyright (c) ivan-mezhenin. All rights reserved.
+// </copyright>
+
+namespace ThreadPool;
+
+public class MyThreadPool
+{
+}

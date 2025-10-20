3b0354ca85a36dc01e75b2583c21d8afbe151194
ivan-mezhenin <mezhenin_ivan_06@mail.ru>
2025-10-20 18:52:28 +0300

    feat: server class

---
+++ Homework4/MyFtp/Server.cs
@@ -0,0 +1,76 @@
+// <copyright file="Server.cs" company="ivan-mezhenin">
+// Copyright (c) ivan-mezhenin. All rights reserved.
+// </copyright>
+
+namespace MyFtp;
+
+using System.Net;
+using System.Net.Sockets;
+
+/// <summary>
+/// server.
+/// </summary>
+public class Server
+{
+    private readonly int port;
+    private TcpListener? listener;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Server"/> class.
+    /// </summary>
+    /// <param name="port">the port on which the server will be running.</param>
+    public Server(int port)
+    {
+        if (port is < 1 or > 65535)
+        {
+            throw new ArgumentException("Port must be between 1 and 65535");
+        }
+
+        this.port = port;
+    }
+
+    /// <summary>
+    /// to start server.
+    /// </summary>
+    /// <returns>nothing.</returns>
+    public async Task Start()
+    {
+        try
+        {
+            this.listener = new TcpListener(IPAddress.Any, this.port);
+
+            try
+            {
+                this.listener.Start();
+            }
+            catch (SocketException ex)
+            {
+                throw new InvalidOperationException("Could not start server", ex);
+            }
+
+            while (true)
+            {
+                try
+                {
+                    var socket = await this.listener.AcceptSocketAsync();
+                }
+                catch (ObjectDisposedException)
+                {
+                    break;
+                }
+            }
+        }
+        finally
+        {
+            this.listener?.Stop();
+        }
+    }
+
+    /// <summary>
+    /// to stop listen server.
+    /// </summary>
+    public void Stop()
+    {
+        this.listener?.Stop();
+    }
+}
